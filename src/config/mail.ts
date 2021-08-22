68cec5967db50eb4031727174daa8a63ebcea061
Rodrigo de França <howdrigo.fr@gmail.com>
2021-08-22 17:20:22 -0300

    feat: adicionado aws-sdk e provider para envio de emails com aws ses

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailProvider {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    }
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: '',
+      name: '',
+    },
+  },
+} as IMailProvider;
